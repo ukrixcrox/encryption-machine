1e41fd499f6bab9f116c0aa8346aa6047dfb46e0
Falk-Witte <falk.witte15@gmail.com>
2022-08-08 03:10:31 +0200

    fixed rot13 algorithm and writing hex thingy

--- test.py
+++
@@ -1,4 +0,0 @@
-f = "Hello, World!"
-
-rot13 = lambda x: x.translate(str.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz","NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"))
-print(rot13(f))
